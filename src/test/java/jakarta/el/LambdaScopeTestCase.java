f118ca843730aa214f5bf81b396e36893ae8801b
Scott M Stark <starksm64@gmail.com>
2019-12-13 12:25:53 -0600

    Initial work to move javax to jakarta package
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

---
+++ src/test/java/jakarta/el/LambdaScopeTestCase.java
@@ -0,0 +1,27 @@
+package jakarta.el;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * testcase for https://issues.jboss.org/browse/JBEE-158
+ *
+ * @author Tomaz Cerar (c) 2015 Red Hat Inc.
+ */
+
+public class LambdaScopeTestCase {
+
+    @Test
+    public void testScope() {
+        ELProcessor processor = new ELProcessor();
+        processor.defineBean("x", null);
+        try {
+            processor.eval("(x -> x.bug ()) ('bug')");
+        } catch (RuntimeException exception) {
+            // This is expected, there is no method bug() on strings.
+
+        }
+        Assert.assertNull(processor.eval("x")); // This must evaluate to null, but instead evaluates to "bug".
+
+    }
+}
---
+++ src/test/java/jakarta/el/NullParameterTestCase.java
@@ -0,0 +1,34 @@
+package jakarta.el;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Testcase for https://issues.jboss.org/browse/WFLY-3456
+ *
+ * @author Trond G. Ziarkowski
+ */
+public class NullParameterTestCase {
+
+    public static interface SimpleDataTableHandler<T> {
+        public Date getLastUpdate(T item);
+    }
+  
+    public static class SimpleStringHandler implements SimpleDataTableHandler<String> {
+        @Override
+        public Date getLastUpdate(String item) {
+            return new Date();
+        }
+    }
+
+    @Test
+    public void testAmbiguousMethodCall() {
+        ELProcessor processor = new ELProcessor();
+        processor.defineBean("handler", new SimpleStringHandler());
+        processor.eval("date = handler.getLastUpdate(null)");
+
+        Assert.assertNotNull(processor.eval("date"));
+    }
+}
